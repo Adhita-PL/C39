506946138c3c5484c60619497eefd348c225b3b1
adhitapl18 <adhitapl18@gmail.com>
2020-10-26 16:34:55 +0530

    Added Images in the game

--- js/Game.js
+++ js/Game.js
@@ -34,6 +34,10 @@ class Game {
     car3 = createSprite(500,200);
     car4 = createSprite(700,200);
     cars = [car1, car2, car3, car4];
+    car1.addImage("car1",car1Img);
+    car2.addImage("car2",car2Img);
+    car3.addImage("car3",car3Img);
+    car4.addImage("car4",car4Img);
   }
 
   play(){
@@ -42,13 +46,15 @@ class Game {
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
+      background("#c68767");
+      image(track,0,-displayHeight*4,displayWidth,displayHeight*5);
       //var display_position = 100;
       
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175;
       var y;
 
       for(var plr in allPlayers){
@@ -56,7 +62,7 @@ class Game {
         index = index + 1 ;
 
         //position the cars a little away from each other in x direction
-        x = x + 200;
+        x = x + 300;
         //use data form the database to display the cars in y direction
         y = displayHeight - allPlayers[plr].distance;
         cars[index-1].x = x;
@@ -69,7 +75,8 @@ class Game {
         }
        
         //textSize(15);
-        //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+        //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position);
+        console.log(allPlayers[plr].distance);
       }
 
     }
@@ -78,7 +85,9 @@ class Game {
       player.distance +=10
       player.update();
     }
-
+    if(player.distance > 5510) {
+      gameState = 2;
+    }
     drawSprites();
   }
 }
